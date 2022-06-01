2ac01806c8bba01c8a0634119016c667d1327766
angelDariaux <angeldariaux@gmail.com>
2022-05-31 20:11:25 -0600

    agregados comentarios, haciendo refactor cuando tenemos un error en el codigo

--- demos/src/02Math.js
+++ demos/src/02Math.js
@@ -7,6 +7,11 @@ const multiply = (a, b) => {
 }
 
 const divide = (a, b) => {
+  if (b === 0 ) {       // todo: Este es el refactor porque no se pueden hacer diviciones por 0.
+
+    return null
+  }
+
   return a / b;
 }
 
--- demos/src/02Math.test.js
+++ demos/src/02Math.test.js
@@ -2,6 +2,28 @@ const  { sum, multiply, divide } = require('./02Math');
 
 test("adds 1 + 3 should be 4", () => {
   const rta = sum(1, 3);
-  expect(rta).toBe(4);
+  expect(rta).toBe(4); // ? expect(rta).toBe(4) Esta es la condicional para saber si la funcion pasa la prueba
 });
 
+test ('should be 4', () => {
+  const rta = multiply(1, 4);
+
+  expect(rta).toBe(4);
+})
+
+test ('should divide', () => {
+  const rta = divide(6, 3);
+  expect(rta).toBe(2);
+
+  const rta2 = divide(5, 2);
+  expect(rta2).toBe(2.5);
+
+})
+
+test ('should divide for 0', () => {
+  const rta = divide (6, 0);
+  expect(rta).toBeNull();     // ! Como devuelve un error colocamos un "toBeNull", Es necesario hacer un refactor en el codigo
+
+  const rta2 = divide (4, 0);
+  expect(rta2).toBeNull();
+})
