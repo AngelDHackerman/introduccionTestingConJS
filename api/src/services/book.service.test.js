56dfa1805e7e3b3f8c89f251394583dc65a1910c
angelDariaux <angeldariaux@gmail.com>
2022-06-14 19:32:29 -0600

    creando spys para testing, corregido el bug de length

--- api/src/services/book.service.test.js
+++ api/src/services/book.service.test.js
@@ -7,14 +7,15 @@ const fakeBooks = [
   },
 ];
 
+const mockGetAll = jest.fn(); // ? asi se hace las pruebas de caja blanca
+
 // ? Esto de abajo nos ayuda a hacer la suplantacion de la base de datos.
-const MongoLibStub = {
-  getAll: () => [...fakeBooks],
-  create: () => {},
-};
 
 // ? La linea de abajo es para no hacer pruebas en la base de datos real.
-jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => MongoLibStub));
+jest.mock('../lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
+  getAll: mockGetAll,
+  create: () => {},
+})));
 
 describe('Test for BookService', () => {
   let service;
@@ -26,11 +27,28 @@ describe('Test for BookService', () => {
   describe('test for getBooks', () => {
     test('should return a list book', async () => {
       // Arrange
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
       const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books.lenght).toEqual(1);
+      expect(books.length).toEqual(1);
+      expect(mockGetAll).toHaveBeenCalled(); // Ve si el spy fue llamado.
+      expect(mockGetAll).toHaveBeenCalledTimes(1);
+      expect(mockGetAll).toHaveBeenCalledWith('books', {});
+    });
+
+    test('should return a list book', async () => {
+      // Arrange
+      mockGetAll.mockResolvedValue([{
+        _id: 1,
+        name: 'Harry Potter 2',
+      }]);
+      // Act
+      const books = await service.getBooks({});
+      console.log(books, 'harry potter 2');
+      // Assert
+      expect(books[0].name).toEqual('Harry Potter 2');
     });
   });
 });
